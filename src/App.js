5d3fd6755905a865e92f381a868b71e60429425f
Abdullah Al Noman <53597501+kazinoman@users.noreply.github.com>
2022-01-03 01:35:35 +0600

    Revert "Add: all page and setup mui theme and route implementation."

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
-import React from "react";
-import { ThemeProvider } from "@mui/material/styles";
-import { Grid } from "@mui/material";
-
 import "./App.css";
-import theme from "./components/theme";
-import PageRoutes from "./pages/routes";
-import Nav from "./components/nav";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Nav />
-      <Grid container>
-        {/* Menu item  */}
-        <Grid item xs={2}>
-          <h4>List item</h4>
-        </Grid>
-        {/* Main page information */}
-        <Grid item xs={10}>
-          <PageRoutes />
-        </Grid>
-      </Grid>
-    </ThemeProvider>
+    <div className="App">
+      <h1>Hello from window.</h1>
+      <h2>Header component</h2>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import reactDom from "react-dom";
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
 const root = document.getElementById("root");
 
-reactDom.render(
-  <Router>
-    <App />
-  </Router>,
-  root
-);
+reactDom.render(<App />, root);
