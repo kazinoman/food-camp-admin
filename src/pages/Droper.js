19a977c0955eaea080df43632d5c2849b896f376
Abdullah Al Noman <kazinoman9999@gmail.com>
2022-01-03 01:17:08 +0600

    Add: all page and setup mui theme and route implementation.

---
+++ src/pages/Droper.js
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const Droper = () => {
+  return (
+    <div>
+      <Typography>Droper page!</Typography>
+    </div>
+  );
+};
+
+export default Droper;
---
+++ src/pages/Settings.js
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const Settings = () => {
+  return (
+    <div>
+      <Typography>Setting page !</Typography>
+    </div>
+  );
+};
+
+export default Settings;
